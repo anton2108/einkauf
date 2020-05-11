45bcefd2655b37990b9c611363ca3439d488050d
anton2108 <32981737+anton2108@users.noreply.github.com>
2020-05-11 15:27:09 +0200

    Update ExampleUnitTest.java

--- app/src/test/java/com/nikotin/menueinkauf/ExampleUnitTest.java
+++ app/src/test/java/com/nikotin/menueinkauf/ExampleUnitTest.java
@@ -15,3 +15,13 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 }
+public class TestMenuNormal(){
+ public void testGetName(){
+	assertThat("", is(sameInstance( "" )));
+	assertThat(new Object(), is(notNullValue()));
+	}
+	public void TestgetMenuId(){
+	assertEquals(2, menuId.getMenuId());
+	assertTrue(menuId.getMenuId() > 0)
+	}
+}
